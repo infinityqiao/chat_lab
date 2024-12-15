b35653501746c54969a81573eeafc06e1219748a
infinity <1833177514@qq.com>
2024-12-15 17:19:28 +0800

    implement singleton httpmgr

---
+++ client/httpmgr.cpp
@@ -0,0 +1,44 @@
+#include "httpmgr.h"
+
+HttpMgr::~HttpMgr()
+{
+
+}
+
+HttpMgr::HttpMgr() {
+    connect(this, &HttpMgr::sig_http_finish, this, &HttpMgr::slot_http_finish);
+}
+
+void HttpMgr::PostHttpReq(QUrl url, QJsonObject json, ReqId req_id, Modules mod)
+{
+    QByteArray data = QJsonDocument(json).toJson();
+    QNetworkRequest request(url);
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+    request.setHeader(QNetworkRequest::ContentLengthHeader, QByteArray::number(data.length()));
+    auto self = shared_from_this();
+    QNetworkReply * reply = _manager.post(request, data);
+    QObject::connect(reply, &QNetworkReply::finished, [self, reply, req_id, mod](){
+        // error
+        if(reply->error() != QNetworkReply::NoError){
+            qDebug() << reply->errorString();
+            emit self->sig_http_finish(req_id, "", ErrorCodes::ERR_NETWORK, mod);
+            reply->deleteLater();
+            return;
+        }
+
+        // success
+        QString res = reply->readAll();
+        emit self->sig_http_finish(req_id, res, ErrorCodes::SUCCESS, mod);
+        reply->deleteLater();
+        return;
+    });
+
+}
+
+void HttpMgr::slot_http_finish(ReqId id, QString res, ErrorCodes err, Modules mod)
+{
+    if (mod == Modules::REGISTERMOD) {
+        // 发送信号通知指定模块 http 的响应结束
+        emit sig_reg_mod_finish(id, res, err);
+    }
+}
--- client/registerdialog.cpp
+++ client/registerdialog.cpp
@@ -1,6 +1,7 @@
 #include "registerdialog.h"
 #include "ui_registerdialog.h"
 #include "global.h"
+#include "httpmgr.h"
 
 RegisterDialog::RegisterDialog(QWidget *parent)
     : QDialog(parent)
@@ -11,6 +12,9 @@ RegisterDialog::RegisterDialog(QWidget *parent)
     ui->confirm_edit->setEchoMode(QLineEdit::Password);
     ui->error_tip->setProperty("state", "normal");
     repolish(ui->error_tip);
+    connect(HttpMgr::GetInstance().get(), &HttpMgr::sig_reg_mod_finish, this, &RegisterDialog::slot_reg_mod_finish);
+
+    initHttpHandlers();
 }
 
 RegisterDialog::~RegisterDialog()
@@ -31,9 +35,48 @@ void RegisterDialog::on_get_code_btn_clicked()
     }
 }
 
+void RegisterDialog::slot_reg_mod_finish(ReqId id, QString res, ErrorCodes err)
+{
+    if(err != ErrorCodes::SUCCESS) {
+        showTip(tr("网络请求错误"), false);
+        return;
+    }
+
+    // 解析 JSON 字符串
+    QJsonDocument jsonDoc = QJsonDocument::fromJson(res.toUtf8());
+    if(jsonDoc.isNull()) {
+        showTip(tr("json解析失败"), false);
+        return;
+    }
+    // JSON 解析错误
+    if(!jsonDoc.isObject()){
+        showTip(tr("json解析失败"), false);
+        return;
+    }
+
+    _handlers[id](jsonDoc.object());
+    return;
+}
+
+void RegisterDialog::initHttpHandlers()
+{
+    // 注册获取验证码回包的逻辑
+    _handlers.insert(ReqId::ID_GET_VARIFY_CODE, [this](const QJsonObject& jsonObj){
+        int error = jsonObj["error"].toInt();
+        if(error != ErrorCodes::SUCCESS){
+            showTip(tr("参数错误"), false);
+            return;
+        }
+
+        auto email = jsonObj["email"].toString();
+        showTip(tr("验证码已发送到邮箱，注意查收"), true);
+        qDebug() << "email is" << email;
+    });
+}
+
 void RegisterDialog::showTip(QString str, bool b_ok)
 {
-    if (b_ok) {
+    if(b_ok){
         ui->error_tip->setProperty("state", "normal");
     } else {
         ui->error_tip->setProperty("state", "error");
