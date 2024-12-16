57fc1197d892198f03f8089d9daf3d832f97aeda
infinity <1833177514@qq.com>
2024-12-16 21:08:50 +0800

    http RegPost

--- server/GateServer/HttpConnection.h
+++ server/GateServer/HttpConnection.h
@@ -12,6 +12,7 @@ private:
 	void CheckDeadline();
 	void WriteResponse();
 	void HandleReq();
+	void PreParseGetParam();
 	tcp::socket _socket;
 	beast::flat_buffer _buffer{ 8192 };
 	http::request<http::dynamic_body> _request;
@@ -19,5 +20,8 @@ private:
 	net::steady_timer deadline_{
 		_socket.get_executor(), std::chrono::seconds(60)
 	};
+
+	std::string _get_url;
+	std::unordered_map<std::string, std::string> _get_params;
 };
 
